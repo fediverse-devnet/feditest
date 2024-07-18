6430c414d1edba16cfceca91551c83e1719f1d86
Johannes Ernst <git@j12t.org>
2024-07-18 13:48:14 -0700

    Instead of limiting invoking ubos-admin over ssh, we can make it a general-purpose mechanism that also works for mechanisms like "machinectl shell" or ssh with non-standard flags.

--- src/feditest/ubos/__init__.py
+++ src/feditest/ubos/__init__.py
@@ -11,11 +11,11 @@ from feditest.reporting import info
 from feditest.utils import hostname_validate, ssh_uri_validate
 
 """
-There is no UbosNode: it only needs to carry sshuri and identity_file (both optional) and having an entire
+There is no UbosNode: it only needs to carry rshcmd (optional) and having an entire
 separate class that needs to be woven into the rest of the class hierarchy as a mixin or as a second
 supertype does not appear to be worth it.
 
-Instead we keep those two values in the parameters dict, where they come from anyway.
+Instead we keep this value in the parameters dict, where it comes from anyway.
 """
 
 
@@ -27,20 +27,23 @@ class UbosNodeDriver(NodeDriver):
         """
         The UBOS driver knows how to provision a node either by deploying a UBOS Site JSON file
         with "ubos-admin deploy" or to restore a known state of a Site with "ubos-admin restore".
-        It performs these operations locally, or if ssh information is provided, remotely over ssh.
+        It performs these operations locally, or if rshcmd information is provided, remotely.
+        rshcmd is simply prepended to the ubos-admin command, so it could be something like:
+        * `ssh user@host`
+        * `ssh -i identity-file user@host`
+        * `sudo machinectl shell ubosdev@container`
+        * Because of its generality, the syntax of this parameter is not validated.
         """
         if not parameters:
             raise NodeSpecificationInsufficientError(self, 'No parameters given')
 
-        sshuri = parameters.get('sshuri')
-        identity_file = parameters.get('identity_file')
-        if sshuri:
-            ssh_uri_validate(sshuri) # will raise
-            if self._exec_shell('which ubos-admin', sshuri, identity_file):
-                raise OSError(f'{ type(self).__name__ } with an sshuri requires UBOS Gears on the remote system (see ubos.net).')
+        rshcmd = parameters.get('rshcmd')
+        if rshcmd:
+            if self._exec_shell('which ubos-admin', rshcmd):
+                raise OSError(f'{ type(self).__name__ } with an rshcmd requires UBOS Gears on the remote system (see ubos.net).')
         else:
             if not shutil.which('ubos-admin'):
-                raise OSError(f'{ type(self).__name__ } without an sshuri requires a local system running UBOS Gears (see ubos.net).')
+                raise OSError(f'{ type(self).__name__ } without an rshcmd requires a local system running UBOS Gears (see ubos.net).')
 
         # FIXME: reconcile provided hostname with what's in the site json / backup
 
@@ -78,27 +81,19 @@ class UbosNodeDriver(NodeDriver):
 
     def _unprovision_node(self, node: Node) -> None:
         parameters = node._parameters
-        sshuri = parameters.get('sshuri')
-        identity_file = parameters.get('identity_file')
+        rshcmd = parameters.get('rshcmd')
 
-        self._exec_shell(
-            f"sudo ubos-admin undeploy --siteid { node.parameter('siteid') }",
-            sshuri,
-            identity_file
-        )
+        self._exec_shell( f"sudo ubos-admin undeploy --siteid { node.parameter('siteid') }", rshcmd )
 
 
-    def _exec_shell(self, cmd: str, sshuri: str | None = None, identity_file: str | None = None):
+    def _exec_shell(self, cmd: str, rshcmd: str | None = None):
         """
-        Invoke a shell command either locally or remotely over ssh.
+        Invoke a shell command either locally or remotely over rshcmd.
         This is defiend on UbosNodeDriver, not UbosNode, so we can invoke it before attempting to
         instantiate the UbosNode.
         """
-        if sshuri:
-            fullcmd = 'ssh'
-            if identity_file:
-                fullcmd += f' -i "{ identity_file }"'
-            fullcmd += f' "{ sshuri }"'
+        if rshcmd:
+            fullcmd = rshcmd
             fullcmd += cmd
         else:
             fullcmd = cmd
